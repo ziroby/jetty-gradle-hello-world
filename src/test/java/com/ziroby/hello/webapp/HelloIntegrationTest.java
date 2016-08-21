62384cd79afd5e982e25d511cbb23b23824b3447
Ron Ziroby Romero <ziroby@ziroby.com>
2016-08-21 17:16:25 +0100

    updated to latest version of jetty

--- src/test/java/com/ziroby/hello/webapp/HelloIntegrationTest.java
+++ src/test/java/com/ziroby/hello/webapp/HelloIntegrationTest.java
@@ -3,20 +3,21 @@ package com.ziroby.hello.webapp;
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
 
-import com.sun.jersey.api.client.Client;
-import com.sun.jersey.api.client.WebResource;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
 
 import org.junit.Test;
 
 public class HelloIntegrationTest {
 	private static String HELLO_URL = "http://localhost:8080/hello";
-	
+
 	@Test
 	public void testHello() throws Exception {
-		Client client = Client.create();
-		WebResource webResource = client.resource(HELLO_URL);
-		String response = webResource.get(String.class);
-		
+		Client client = ClientBuilder.newClient();
+		WebTarget webTarget = client.target(HELLO_URL);
+		String response = webTarget.request().get(String.class);
+
 		assertThat(response, is("Hello, World!"));
 	}
 }
